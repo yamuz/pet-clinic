39f264d0ea55310a5475d79cbbcbfc9fb742aad5
Almaz <Yamuuzz@gmail.com>
2021-11-23 10:27:59 +0300

    closes #1.

--- src/main/java/guru/spring/petclinic/PetClinicApplication.java
+++ src/main/java/guru/spring/petclinic/PetClinicApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetClinicApplication {
 
+    //added closes #1.
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
