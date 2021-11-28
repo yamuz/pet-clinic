6e6cac4c2606485a4f6e0d98c403bed775408511
Almaz <Yamuuzz@gmail.com>
2021-11-28 17:21:46 +0300

    -feat. add owner cntrlr

---
+++ pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owner")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/index","/index.html"})
+    public String indexFun(){
+        return "owner/index";
+    }
+}
