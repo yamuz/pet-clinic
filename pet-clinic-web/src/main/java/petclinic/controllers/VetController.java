07b3643311d9e37f1c1f0f52da4da72633e0658a
Almaz <Yamuuzz@gmail.com>
2021-11-29 15:16:00 +0300

    -feat. added listing of vets to html. closes #12

--- pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
@@ -3,13 +3,21 @@ package petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import petclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vet","vet/index","vet/index.html"})
     public String indexFun(Model model){
 
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
